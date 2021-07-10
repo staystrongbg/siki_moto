fabc6c733d6c45b24221f12ea895f4a552364037
ddamaja <79502735+staystrongbg@users.noreply.github.com>
2021-07-10 22:39:10 +0200

    Add files via upload

--- src/Footer.js
+++ src/Footer.js
@@ -11,18 +11,18 @@ const Footer = () => {
 
   return (
     <footer>
-      <div id='desni'>
-        <p>
-          Moto oprema "Grada"
-          <br />
-          Adresa: Ruzveltova 36, Zvezdara, Beograd
-          <br />
-          Broj telefona:+381 65555888
-          <br />
-          E-mail:moto-oprema@gmail.com
-        </p>
-      </div>
       <div className='map-responsive'>
+        <div id='desni'>
+          <p>
+            Moto oprema "Grada"
+            <br />
+            Adresa: Ruzveltova 36, Zvezdara, Beograd
+            <br />
+            Broj telefona:+381 65555888
+            <br />
+            E-mail:moto-oprema@gmail.com
+          </p>
+        </div>
         <iframe
           title='gmap'
           width='100%'
